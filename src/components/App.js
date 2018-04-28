28ace91bbb58d14934dec5592f8a46e3842590dc
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2018-04-28 18:44:04 +0300

    two movies

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,92 @@
-import React, { Component } from "react";
-import movie from "./movie";
+import React from "react";
+import movies from "./movies";
 
-function MovieItem(props) {
-  const {
-    item = {
-      title: "React"
-    }
-  } = props;
-  return (
-    <div className="card" style={{ width: "300px" }}>
-      <img className="card-img-top" src={item.backdrop_path} alt="" />
-      <div className="card-body">
-        <h6 className="card-title">{item.title}</h6>
-        <div className="d-flex justify-content-between align-items-center">
+class MovieItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isShowOverview: false
+    };
+  }
+
+  render() {
+    const { item } = this.props;
+    // console.log("state of MovieItem", this.state);
+    // console.log("props", this.props);
+    console.log("render");
+    return (
+      <div className="card" style={{ width: "300px" }}>
+        <img className="card-img-top" src={item.backdrop_path} alt="" />
+        <div className="card-body">
+          <h6 className="card-title">{item.title}</h6>
           <p className="mb-0">Rating: {item.vote_average}</p>
+          <button
+            onClick={() => {
+              this.setState({
+                isShowOverview: true
+              });
+            }}
+          >
+            Show overview
+          </button>
+          <button
+            onClick={() => {
+              this.setState({
+                isShowOverview: false
+              });
+            }}
+          >
+            Hide overview
+          </button>
+          {this.state.isShowOverview ? <p>{item.overview}</p> : null}
+          <button onClick={this.props.addLike}>Like</button>
+          {this.props.isLike ? <p>Это кино мне нравится</p> : null}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-function App() {
-  return (
-    <div>
-      <MovieItem item={movie} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: movies,
+      isLike: false
+    };
+  }
+
+  addLike = () => {
+    this.setState({
+      isLike: true
+    });
+  };
+
+  render() {
+    console.log("state of App", this.state);
+    return (
+      <div
+        style={{
+          display: "flex",
+          widht: "100%",
+          justifyContent: "space-between"
+        }}
+      >
+        <MovieItem
+          item={this.state.items[0]}
+          addLike={this.addLike}
+          isLike={this.state.isLike}
+        />
+        <MovieItem
+          item={this.state.items[1]}
+          addLike={this.addLike}
+          isLike={this.state.isLike}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
