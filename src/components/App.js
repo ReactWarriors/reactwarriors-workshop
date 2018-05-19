cca5764510b362717aba08bcb3e901013cdb37a0
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2018-05-19 16:25:35 +0300

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+function MovieItem(props) {
+  console.log("props", props);
+  return (
+    <div className="card">
+      <img className="card-img-top" src="" alt="" />
+      <div className="card-body">
+        <h6 className="card-title">{props.title}</h6>
+        <div className="d-flex justify-content-between align-items-center">
+          <p className="mb-0">Rating: {props.rating}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 function App() {
-  return <div>React Warriors Hello!</div>;
+  return (
+    <div>
+      <MovieItem title="Main title" rating={1} like={true} />
+    </div>
+  );
 }
 
 export default App;
