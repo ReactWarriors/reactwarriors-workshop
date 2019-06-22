5926213e741bc39f436cc58cd8770ee85771bc58
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2019-06-22 13:32:30 +0300

    initial 22.06

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./stylesheets/index.scss";
 
-const example = 1;
 ReactDOM.render(<App />, document.getElementById("root"));
