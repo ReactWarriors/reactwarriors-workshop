bffaaca49ed366f9d7f059e08a8500e1a569fbbe
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2019-06-23 18:43:43 +0300

    base login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,61 @@
 import React from "react";
+import { API_URL, API_KEY_3 } from "../utils/api";
 
 export default class LoginForm extends React.Component {
+  constructor() {
+    super();
+
+    this.inputNameRef = React.createRef();
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+  componentDidMount() {
+    // console.log(this.refs.inputNameRef);
+    // this.refs.inputNameRef.focus();
+    // console.log(this.inputNameRef);
+    this.inputNameRef.current.focus();
+  }
+
+  // onChangeUsername = event => {
+  //   console.log(event.target.name, event.target.value);
+  //   this.setState({
+  //     username: event.target.value
+  //   });
+  // };
+
+  // onChangePassword = event => {
+  //   console.log(event.target.name, event.target.value);
+  //   this.setState({
+  //     password: event.target.value
+  //   });
+  // };
+
+  onChange = event => {
+    console.log(event.target.name, event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    // console.log(this.refs.inputNameRef.name, this.refs.inputNameRef.value);
+
+    fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log("data", data);
+      });
+  };
+
   render() {
     return (
       <div className="form-login-container">
-        <form className="form-login">
+        <form className="form-login" onSubmit={this.onSubmit}>
           <h1 className="h3 mb-3 font-weight-normal text-center">
             Авторизация
           </h1>
@@ -16,6 +67,9 @@ export default class LoginForm extends React.Component {
               id="username"
               placeholder="Пользователь"
               name="username"
+              value={this.state.username}
+              ref={this.inputNameRef}
+              onChange={this.onChange}
             />
           </div>
           <div className="form-group">
@@ -26,6 +80,8 @@ export default class LoginForm extends React.Component {
               id="password"
               placeholder="Пароль"
               name="password"
+              value={this.state.password}
+              onChange={this.onChange}
             />
           </div>
           <button type="submit" className="btn btn-lg btn-primary btn-block">
---
+++ src/components/MoviesPage.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import MovieItem from "./MovieItem";
+import { API_URL, API_KEY_3 } from "../utils/api";
+import MovieTabs from "./MovieTabs";
+
+class MoviesPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      moviesWillWatch: [],
+      sort_by: "popularity.desc"
+    };
+  }
+
+  componentDidMount() {
+    console.log("MoviesPage didMount");
+    // const _this = this
+    this.getMovies();
+    // console.log("after fetch")
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("MoviesPage didUpdate");
+    // console.log("prev", prevProps, prevState);
+    // console.log("this", this.props, this.state);
+    if (prevState.sort_by !== this.state.sort_by) {
+      console.log("MoviesPage call api");
+      this.getMovies();
+    }
+  }
+
+  getMovies = () => {
+    fetch(
+      `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${
+        this.state.sort_by
+      }`
+    )
+      .then(response => {
+        // console.log("then", response);
+        return response.json();
+      })
+      .then(data => {
+        // console.log("data", data);
+        this.setState({
+          movies: data.results
+        });
+      });
+  };
+
+  deleteMovie = movie => {
+    console.log(movie.id);
+    const updateMovies = this.state.movies.filter(item => item.id !== movie.id);
+    console.log(updateMovies);
+
+    // this.state.movies = updateMovies;
+    this.setState({
+      movies: updateMovies
+    });
+  };
+
+  addMovieToWillWatch = movie => {
+    const updateMoviesWillWatch = [...this.state.moviesWillWatch];
+    updateMoviesWillWatch.push(movie);
+
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch
+    });
+  };
+
+  deleteMovieFromWillWatch = movie => {
+    const updateMoviesWillWatch = this.state.moviesWillWatch.filter(
+      item => item.id !== movie.id
+    );
+
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch
+    });
+  };
+
+  updateSortBy = value => {
+    this.setState({
+      sort_by: value
+    });
+  };
+
+  render() {
+    console.log("MoviesPage render", this.state.sort_by);
+    return (
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-9">
+            <div className="row mb-4">
+              <div className="col-12">
+                <MovieTabs
+                  sort_by={this.state.sort_by}
+                  updateSortBy={this.updateSortBy}
+                />
+              </div>
+            </div>
+            <div className="row">
+              {this.state.movies.map(movie => {
+                return (
+                  <div className="col-6 mb-4" key={movie.id}>
+                    <MovieItem
+                      data={movie}
+                      deleteMovie={this.deleteMovie}
+                      addMovieToWillWatch={this.addMovieToWillWatch}
+                      deleteMovieFromWillWatch={this.deleteMovieFromWillWatch}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="col-3">
+            <h4>Will Watch: {this.state.moviesWillWatch.length} movies</h4>
+            <ul className="list-group">
+              {this.state.moviesWillWatch.map(movie => (
+                <li key={movie.id} className="list-group-item">
+                  <div className="d-flex justify-content-between">
+                    <p>{movie.title}</p>
+                    <p>{movie.vote_average}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MoviesPage;
